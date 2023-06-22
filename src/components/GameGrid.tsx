68b7b7fc24134a5304a007751bb5f8b0aa73d01e
GnaneshNayak <mr.gnaneshnayak@gmail.com>
2023-06-23 03:07:06 +0530

    Displaying genres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -13,7 +13,7 @@ const GameGrid = () => {
       <Text color={'#EF5350'}>{error && <p>{error}</p>}</Text>
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
-        spacing={10}
+        spacing={3}
         padding={'10px'}
       >
         {isLoading &&
