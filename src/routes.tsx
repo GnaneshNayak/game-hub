51ea11b674b2239e43411806772752f474d19bc8
GnaneshNayak <mr.gnaneshnayak@gmail.com>
2023-07-25 16:50:29 +0530

    hande errors

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,11 +3,13 @@ import { createBrowserRouter } from 'react-router-dom';
 import Layouts from './pages/Layouts';
 import Homepage from './pages/Homepage';
 import GameDetailPage from './pages/GameDetailPage';
+import ErrorPage from './pages/ErrorPAge';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Layouts />,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <Homepage /> },
       { path: 'games/:id', element: <GameDetailPage /> },
